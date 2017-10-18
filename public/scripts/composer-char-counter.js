45b34972fc1a369469019aed5f774d7af9c9bdfd
Shawn Griffin <shawn@shawngriffin.com>
2017-10-18 08:52:21 -0700

    fixed tweet input form scrolling textarea bug, waiting to do XSS fix

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,15 +6,23 @@ const cMaxTweetLength = 140;
 * if -'ve turns .Counter red
 */
 
+
 $(document).ready(function() {
   $("#tweetText").on("keyup", function() {
-    const tweetLength = cMaxTweetLength - $(this).val().length;
+    
+    var tweetString = $(this).val();
+
+    const tweetLength = cMaxTweetLength - tweetString.length;
+
     // go to the parent, find the .counter, set it to space remaing and red if less than cMaxTweetLength.
-    const counter = $(this).parent().find(".counter");
+    const counter = $(this)
+      .parent()
+      .find(".counter");
+
     if (tweetLength < 0) {
       counter.addClass("error");
     } else {
-     counter.removeClass("error");
+      counter.removeClass("error");
     }
     counter.html(tweetLength);
   });
